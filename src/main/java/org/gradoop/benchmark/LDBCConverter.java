70b57ec69ea30cb36f957bd00856f976787a4b35
Martin Junghanns <m.junghanns@mailbox.org>
2016-01-09 12:15:43 +0100

    added merge method

--- src/main/java/org/gradoop/benchmark/LDBCConverter.java
+++ src/main/java/org/gradoop/benchmark/LDBCConverter.java
@@ -1,25 +1,30 @@
 package org.gradoop.benchmark;
 
 import org.apache.flink.api.common.ProgramDescription;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.operators.MapOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.gradoop.model.api.EPGMEdgeFactory;
 import org.gradoop.model.api.EPGMVertexFactory;
 import org.gradoop.model.impl.EPGMDatabase;
 import org.gradoop.model.impl.LogicalGraph;
+import org.gradoop.model.impl.functions.epgm.Id;
+import org.gradoop.model.impl.functions.epgm.SourceId;
 import org.gradoop.model.impl.id.GradoopId;
 import org.gradoop.model.impl.operators.summarization.Summarization;
 import org.gradoop.model.impl.operators.summarization.SummarizationStrategy;
-import org.gradoop.model.impl.operators.summarization.functions.aggregation
-  .CountAggregator;
+import org.gradoop.model.impl.operators.summarization.functions.aggregation.CountAggregator;
 import org.gradoop.model.impl.pojo.EdgePojo;
 import org.gradoop.model.impl.pojo.GraphHeadPojo;
 import org.gradoop.model.impl.pojo.VertexPojo;
+import org.gradoop.model.impl.pojo.VertexPojoFactory;
 import org.gradoop.model.impl.properties.PropertyList;
+import org.gradoop.model.impl.properties.PropertyValue;
 import org.gradoop.util.GradoopFlinkConfig;
 import org.s1ck.ldbc.LDBCToFlink;
 import org.s1ck.ldbc.tuples.LDBCEdge;
@@ -49,12 +54,21 @@ public class LDBCConverter implements ProgramDescription {
     case "convert" :
       convertToGradoop(ldbcToFlink, env, args[2]);
       break;
+    case "update" :
+      updatePersons(env, inputDir, args[2]);
+      break;
     case "summarize" :
       summarize(env, inputDir, args[2]);
       break;
     }
   }
 
+  /**
+   * Prints a map (label, count) for vertices.
+   *
+   * @param ldbcToFlink
+   * @throws Exception
+   */
   public static void printLabelStatistics(LDBCToFlink ldbcToFlink) throws
     Exception {
     ldbcToFlink.getVertices()
@@ -69,6 +83,14 @@ public class LDBCConverter implements ProgramDescription {
       .print();
   }
 
+  /**
+   * Converts ldbc2flink data into Gradoop data
+   *
+   * @param ldbcToFlink
+   * @param env
+   * @param outputDir
+   * @throws Exception
+   */
   public static void convertToGradoop(LDBCToFlink ldbcToFlink, ExecutionEnvironment env, String outputDir) throws
     Exception {
     GradoopFlinkConfig<GraphHeadPojo, VertexPojo, EdgePojo> gradoopConf =
@@ -108,6 +130,116 @@ public class LDBCConverter implements ProgramDescription {
       LogicalGraph.fromDataSets(vertices, edges, gradoopConf);
 
     logicalGraph.writeAsJson(
+      outputDir + NODES_JSON,
+      outputDir + EDGES_JSON,
+      outputDir + GRAPHS_JSON
+    );
+  }
+
+  /**
+   * Transforms
+   * person-[isLocatedIn]->city:name = "Leipzig"
+   * to
+   * person.city = "Leipzig"
+   *
+   * @param env
+   * @param inputDir
+   * @param outputDir
+   */
+  @SuppressWarnings("unchecked")
+  public static void updatePersons(ExecutionEnvironment env, String inputDir, String outputDir) throws Exception {
+    GradoopFlinkConfig<GraphHeadPojo, VertexPojo, EdgePojo> gradoopConf =
+      GradoopFlinkConfig.createDefaultConfig(env);
+
+    EPGMDatabase<GraphHeadPojo, VertexPojo, EdgePojo> epgmDatabase =
+      EPGMDatabase.fromJsonFile(
+        inputDir + NODES_JSON,
+        inputDir + EDGES_JSON,
+        inputDir + GRAPHS_JSON,
+        gradoopConf
+      );
+
+    LogicalGraph<GraphHeadPojo, VertexPojo, EdgePojo> personsCitySubgraph =
+      epgmDatabase.getDatabaseGraph()
+        .subgraph(new FilterFunction<VertexPojo>() {
+          @Override
+          public boolean filter(VertexPojo vertexPojo) throws Exception {
+            return vertexPojo.getLabel().equals("person") ||
+              vertexPojo.getLabel().equals("city");
+          }
+        }, new FilterFunction<EdgePojo>() {
+          @Override
+          public boolean filter(EdgePojo edgePojo) throws Exception {
+            return edgePojo.getLabel().equals("isLocatedIn");
+          }
+        });
+
+    DataSet<VertexPojo> updatedVertices =
+      personsCitySubgraph.getVertices()
+        .filter(new FilterFunction<VertexPojo>() {
+          @Override
+          public boolean filter(VertexPojo vertexPojo) throws Exception {
+            return vertexPojo.getLabel().equals("person");
+          }
+        })
+        .join(personsCitySubgraph.getEdges())
+        .where(new Id<VertexPojo>()).equalTo(new SourceId<EdgePojo>())
+        .with(new JoinFunction<VertexPojo, EdgePojo, SourceEdgePair>() {
+          @Override
+          public SourceEdgePair join(VertexPojo vertexPojo, EdgePojo edgePojo) throws
+            Exception {
+            return new SourceEdgePair(vertexPojo, edgePojo);
+          }
+        })
+        .join(personsCitySubgraph.getVertices()
+          .filter(new FilterFunction<VertexPojo>() {
+            @Override
+            public boolean filter(VertexPojo vertexPojo) throws Exception {
+              return vertexPojo.getLabel().equals("city");
+            }
+          }))
+        .where(new KeySelector<SourceEdgePair, GradoopId>() {
+          @Override
+          public GradoopId getKey(SourceEdgePair pair) throws Exception {
+            return pair.getEdge().getTargetId();
+          }
+        })
+        .equalTo(new Id<VertexPojo>())
+        .with(new JoinFunction<SourceEdgePair, VertexPojo, VertexPojo>() {
+
+          final VertexPojoFactory factory = new VertexPojoFactory();
+
+          @Override
+          public VertexPojo join(SourceEdgePair pair, VertexPojo target) throws
+            Exception {
+            VertexPojo source = pair.getSource();
+            PropertyValue cityProperty = target.getPropertyValue("name");
+            if (cityProperty != null) {
+              source.setProperty("city", cityProperty.getObject());
+            }
+
+            return factory.initVertex(source.getId(), source.getLabel(),
+              source.getProperties(), source.getGraphIds());
+          }
+        });
+
+    DataSet<VertexPojo> newVertices =
+      epgmDatabase.getDatabaseGraph().getVertices()
+        .leftOuterJoin(updatedVertices)
+        .where(new Id<VertexPojo>()).equalTo(new Id<VertexPojo>())
+        .with(new JoinFunction<VertexPojo, VertexPojo, VertexPojo>() {
+          @Override
+          public VertexPojo join(VertexPojo left, VertexPojo right) throws
+            Exception {
+            return right == null ? left : right;
+          }
+        });
+
+    LogicalGraph.fromDataSets(
+      newVertices,
+      epgmDatabase.getDatabaseGraph().getEdges(),
+      epgmDatabase.getDatabaseGraph().getConfig())
+    .writeAsJson(
       outputDir + NODES_JSON,
       outputDir + EDGES_JSON,
       outputDir + GRAPHS_JSON
@@ -177,6 +309,32 @@ public class LDBCConverter implements ProgramDescription {
       }
 
       return factory.createEdge(label, sourceId, targetId, props);
+    }
+  }
+
+  public static class SourceEdgePair extends Tuple2<VertexPojo, EdgePojo> {
+
+    public SourceEdgePair() {}
+
+    public SourceEdgePair(VertexPojo source, EdgePojo edge) {
+      f0 = source;
+      f1 = edge;
+    }
+
+    public void setSource(VertexPojo vertexPojo) {
+      f0 = vertexPojo;
+    }
+
+    public VertexPojo getSource() {
+      return f0;
+    }
+
+    public void setEdge(EdgePojo edgePojo) {
+      f1 = edgePojo;
+    }
+
+    public EdgePojo getEdge() {
+      return f1;
     }
   }
 
