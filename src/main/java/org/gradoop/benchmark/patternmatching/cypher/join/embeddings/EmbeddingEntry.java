83289638096fc6d9a78b50a50aba775714cf546b
DarthMax <max@kopfueber.org>
2016-12-08 16:53:15 +0100

    Improve EmbeddingRecord

--- src/main/java/org/gradoop/benchmark/patternmatching/cypher/join/embeddings/EmbeddingEntry.java
+++ src/main/java/org/gradoop/benchmark/patternmatching/cypher/join/embeddings/EmbeddingEntry.java
@@ -1,9 +1,8 @@
 package org.gradoop.benchmark.patternmatching.cypher.join.embeddings;
 
-/**
- * Created by max on 06.12.16.
- */
+
 public interface EmbeddingEntry {
   long getId();
 
+  void setId(long id);
 }
